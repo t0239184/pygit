8caacc56a63b25bfd3b511608a7c8170ec02629d
Alan Chen <alanchen@cherricorp.com>
2021-04-05 20:56:51 +0800

    Refactor Code

--- pygit.py
+++ pygit.py
@@ -10,20 +10,28 @@ from collections.abc import Callable
 
 import git
 
-enable_verbose = False
+ENABLE_VERBOSE = False
+REPOSITORY_PATH = ''
+START_COMMIT_HEX = ''
+END_COMMIT_HEX = ''
+EXPORT_PATH = ''
 
 
 def print_log(msg):
-    global enable_verbose
-    if enable_verbose:
+    """ Print Log By Verbose Flag """
+    if ENABLE_VERBOSE:
         if isinstance(msg, list):
             print(" ".join(msg))
         else:
             print(str(msg))
 
 
-def print_information_from_(commit):
-    print_log('\n-------------------------------------------------')
+def print_information_from_(commit: object):
+    """
+    Print Commit Information
+    include datetime, hex, author, summary, file list
+    """
+    print_log('\n\n')
 
     # Ignore merge branch information
     if 'Merge branch' in commit.summary:
@@ -38,19 +46,17 @@ def print_information_from_(commit):
         # Print commit message
     print_log(f'{commit.committed_datetime}')
     print_log(f'[{commit.hexsha}] {commit.committer}')
-    # print_log(commit.parents)
+
     print_log(commit.summary)
+    print_log(commit.message)
+
+    print_log('type:' + commit.type)
+    print_log(commit.traverse())
     print_log(['   ', '\n    '.join(list(commit.stats.files))])
 
 
-def print_modify_file_path_from_(file_list):
-    print_log('\nModify File List')
-    print_log("=================================================")
-    for f_path in sorted(file_list):
-        print_log(f_path)
-
-
 def print_help():
+    """ Print Help Message To Console """
     print('Arguments:')
     print('    -r --repo       repository path')
     print('    -s --start      start commit hex')
@@ -66,107 +72,207 @@ def print_help():
            '--export {export_path}'])
 
 
-def is_not_blank(value):
+def is_not_blank(value: str):
+    """ Check Function, Check value is not blank """
     return bool(value)
 
 
-def has_value(index):
+def has_value(index: int):
+    """ Check Function """
     return len(sys.argv) > index and sys.argv[index]
 
 
 def is_dir(path: str):
+    """ Check Function """
     if is_base_on_home(path):
         path = convert_home_path(path)
     return os.path.isdir(str(path))
 
 
-def folder_not_exist(path):
-    return not os.path.exists(str(path))
+def folder_exist(path: str):
+    """ Check Function """
+    return os.path.exists(str(path))
 
 
 def check(value: object, func: Callable, err_msg: str):
+    """ Check by check function """
     if not func(value):
         raise SystemExit(err_msg)
 
 
-def is_base_on_home(path):
+def is_base_on_home(path: str):
+    """ determine path is start with home path symble ~ """
     return str(path).startswith('~')
 
 
-def convert_home_path(path):
+def convert_home_path(path: str):
+    """ Convert ~ path to absoulute home path """
     return str(path).replace('~', os.environ['HOME'])
 
 
-def main():
-    if len(sys.argv) <= 1:
-        print_help()
-        sys.exit()
+def print_modify_file_path_from_(file_list):
+    """ Print All Modify File Path """
+    if not file_list:
+        return
+    print_log('\nModify File List')
+    print_log("=================================================")
+    for f_path in sorted(file_list):
+        print_log(['   ', f_path])
 
-    repository_path = ''
-    start_commit_hex = ''
-    end_commit_hex = ''
 
-    # File zip export path
-    export_path = ''
+def print_modify_file_folder_from_(file_list):
+    """ Print all file folder """
+    if not file_list:
+        return file_list
 
-    for i, arg in enumerate(sys.argv[1:], 1):
-        if not arg.startswith('-'):
+    print_log('\nOutput File folder')
+    print_log('=================================================')
+    uni_folder_list = set()
+    for file_path in sorted(file_list):
+        folder = file_path[:-len(file_path.split('/')[-1])]
+        uni_folder_list.add(EXPORT_PATH + folder)
+
+    print_log(['   ', '\n    '.join(uni_folder_list)])
+    return uni_folder_list
+
+
+def generate_folder_structure_by_(uni_folder_list):
+    """ Create taget folder structure """
+    if not uni_folder_list:
+        return
+
+    print_log('\nGenerate Folder')
+    print_log('=================================================')
+    for folder in uni_folder_list:
+        if folder_exist(folder):
             continue
 
-        if arg in ('--repo', '-r'):
-            index = i+1
-            check(index, has_value, err_msg='Repository path not be blank.')
-            check(sys.argv[index], is_dir,
-                  err_msg='The repository path is not a folder.')
-            if not sys.argv[index].endswith('/'):
-                repository_path = convert_home_path(sys.argv[index]) + '/'
+        cmd = f'mkdir -p {str(folder)}'
+        print_log(['   ', cmd])
+        subprocess.call(cmd, shell=True)
+
+
+def copy_file_to_target_by_(file_list):
+    """ Copy source file to taget path """
+    if not file_list:
+        return
+
+    print_log('\nCopy File')
+    print_log('=================================================')
+    for file in file_list:
+        cmd = f'cp {REPOSITORY_PATH + str(file)} {EXPORT_PATH + str(file)}'
+        print_log(['   ', cmd])
+        subprocess.call(cmd, shell=True)
+
+
+def command_line(func):
+    """ Command Line Args Decorator """
+    def wrapper():
+        """ Real CLI Checker """
+        if len(sys.argv) <= 1:
+            print_help()
+            sys.exit()
+
+        global REPOSITORY_PATH
+        global START_COMMIT_HEX
+        global END_COMMIT_HEX
+        global EXPORT_PATH
+        global ENABLE_VERBOSE
+
+        for i, arg in enumerate(sys.argv[1:], 1):
+            if not arg.startswith('-'):
+                continue
+
+            if arg in ('--repo', '-r'):
+                index = i+1
+                check(
+                    value=index,
+                    func=has_value,
+                    err_msg='Repository path not be blank.'
+                )
+                check(
+                    value=sys.argv[index],
+                    func=is_dir,
+                    err_msg='The repository path is not a folder.'
+                )
+                REPOSITORY_PATH = (
+                    convert_home_path(sys.argv[index])
+                    if sys.argv[index].endswith('/')
+                    else convert_home_path(sys.argv[index]) + '/'
+                )
+            elif arg in ('--verbose', '-v'):
+                ENABLE_VERBOSE = True
+
+            elif arg in ('--start', '-s'):
+                index = i+1
+                check(
+                    value=index,
+                    func=has_value,
+                    err_msg='Start commit hex not be blank.'
+                )
+                check(
+                    value=sys.argv[index],
+                    func=is_not_blank,
+                    err_msg='Start commit hex not be blank.'
+                )
+                START_COMMIT_HEX = sys.argv[index]
+
+            elif arg in ('--end', '-e'):
+                index = i+1
+                check(
+                    value=index,
+                    func=has_value,
+                    err_msg='End commit hex not be blank.'
+                )
+                check(
+                    value=sys.argv[index],
+                    func=is_not_blank,
+                    err_msg='End commit hex not be blank.'
+                )
+                END_COMMIT_HEX = sys.argv[index]
+
+            elif arg in ('--out', '-o'):
+                index = i+1
+                check(
+                    value=index,
+                    func=has_value,
+                    err_msg='Export path not be blank.'
+                )
+                check(
+                    value=sys.argv[index],
+                    func=is_not_blank,
+                    err_msg='Export path is not a folder.'
+                )
+                EXPORT_PATH = (
+                    sys.argv[index]
+                    if sys.argv[index].endswith('/')
+                    else sys.argv[index] + '/'
+                )
+
             else:
-                repository_path = convert_home_path(sys.argv[index])
+                raise SystemExit(f'Invalid argument. arg: {arg}')
 
-        elif arg in ('--verbose', '-v'):
-            global enable_verbose
-            enable_verbose = True
+        if (not REPOSITORY_PATH or not START_COMMIT_HEX
+                or not END_COMMIT_HEX or not EXPORT_PATH):
+            sys.exit()
 
-        elif arg in ('--start', '-s'):
-            index = i+1
-            check(index, has_value, err_msg='Start commit hex not be blank.')
-            check(sys.argv[index], is_not_blank,
-                  err_msg='Start commit hex not be blank.')
-            start_commit_hex = sys.argv[index]
+        print_log('\nInput argument')
+        print_log('------------------------------')
+        print_log(['    repo  : ', REPOSITORY_PATH])
+        print_log(['    start : ', START_COMMIT_HEX])
+        print_log(['    end   : ', END_COMMIT_HEX])
+        print_log(['    out   : ', EXPORT_PATH])
 
-        elif arg in ('--end', '-e'):
-            index = i+1
-            check(index, has_value, err_msg='End commit hex not be blank.')
-            check(sys.argv[index], is_not_blank,
-                  err_msg='End commit hex not be blank.')
-            end_commit_hex = sys.argv[index]
+        return func()
+    return wrapper
 
-        elif arg in ('--out', '-o'):
-            index = i+1
-            check(index, has_value, err_msg='Export path not be blank.')
-            check(sys.argv[index], is_not_blank,
-                  err_msg='Export path is not a folder.')
-            if not sys.argv[index].endswith('/'):
-                export_path = sys.argv[index] + '/'
-            else:
-                export_path = sys.argv[index]
 
-        else:
-            raise SystemExit(f'Invalid argument. arg: {arg}')
+@command_line
+def main():
+    """ Main Process """
 
-    print('Input argument')
-    print('------------------------------')
-    print('repo  : ', repository_path)
-    print('start : ', start_commit_hex)
-    print('end   : ', end_commit_hex)
-    print('out   : ', export_path)
-
-    if (not repository_path or not start_commit_hex
-            or not end_commit_hex or not export_path):
-        sys.exit()
-
-    repo = git.Repo(repository_path)
-    commits = list(repo.iter_commits(start_commit_hex))
+    repo = git.Repo(REPOSITORY_PATH)
+    commits = list(repo.iter_commits(START_COMMIT_HEX, max_count=50))
     file_list = set()
 
     for commit in commits:
@@ -179,30 +285,15 @@ def main():
         file_list.update(commit.stats.files)
 
         # Check is last run
-        if commit.hexsha == end_commit_hex:
+        if END_COMMIT_HEX in (commit.hexsha, commit.hexsha[:8]):
             break
 
     print_modify_file_path_from_(file_list)
+    uni_folder_list = print_modify_file_folder_from_(file_list)
 
-    print_log('\nOutput File folder')
-    print_log("=================================================")
-    uni_folder_list = set()
-    for file_path in sorted(file_list):
-        folder = file_path[:-len(file_path.split('/')[-1])]
-        uni_folder_list.add(export_path + folder)
-
-    print_log([('\n'.join(uni_folder_list)), '\n'])
-
-    for folder in uni_folder_list:
-        if folder_not_exist(folder):
-            cmd = f'mkdir -p {str(folder)}'
-            print_log(cmd)
-            subprocess.call(cmd, shell=True)
-
-    for file in file_list:
-        cmd = f'cp {repository_path + str(file)} {export_path + str(file)}'
-        print_log(cmd)
-        subprocess.call(cmd, shell=True)
+    # Export file
+    generate_folder_structure_by_(uni_folder_list)
+    copy_file_to_target_by_(file_list)
 
 
 if __name__ == '__main__':
